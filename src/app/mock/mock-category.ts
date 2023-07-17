2bf31986cb2d676a6b7ef30ca670f13d89b9b35a
anas-devhelp <anas.devhelp@gmail.com>
2023-07-17 17:25:10 +0530

    Segregating dashboard and category

--- src/app/mock/mock-category.ts
+++ src/app/mock/mock-category.ts
@@ -1,9 +1,9 @@
 import { Category } from '../interfaces/Category';
 
 export const CATEGORIES: Category[] =[
-	{ id: 1, title: 'Category A', description:'', imageUrl: '' },
-	{ id: 2, title: 'Category B', description:'', imageUrl: '' },
-	{ id: 3, title: 'Category C', description:'', imageUrl: '' },
-	{ id: 4, title: 'Category D', description:'', imageUrl: '' },
-	{ id: 5, title: 'Category E', description:'', imageUrl: '' },
+	{ id: 1, title: 'Category A', description:'', imageUrl: '', slug:'' },
+	{ id: 2, title: 'Category B', description:'', imageUrl: '', slug:'' },
+	{ id: 3, title: 'Category C', description:'', imageUrl: '', slug:'' },
+	{ id: 4, title: 'Category D', description:'', imageUrl: '', slug:'' },
+	{ id: 5, title: 'Category E', description:'', imageUrl: '', slug:'' },
 ];
